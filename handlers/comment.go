7c7160ef7ac2d1bceb3c6b0a40b056df52279c86
hanapiko <apikojuma94@gmail.com>
2025-02-06 14:26:01 +0300

    comment session

--- handlers/comment.go
+++ handlers/comment.go
@@ -1,10 +1,30 @@
 package handlers
 
 import (
+	"database/sql"
 	"net/http"
+	"time"
 )
 
 func CommentHandler(w http.ResponseWriter, r *http.Request) {
+	var userID string
+	sessionCookie, err := r.Cookie("session_id")
+	if err == nil {
+		err = db.QueryRow("SELECT user_id FROM sessions WHERE session_id = ?", sessionCookie.Value).Scan(&userID)
+		if err == sql.ErrNoRows {
+			http.SetCookie(w, &http.Cookie{
+				Name:     "session_id",
+				Value:    "",
+				Path:     "/",
+				Expires:  time.Unix(0, 0),
+				MaxAge:   -1,
+				HttpOnly: true,
+			})
+		} else if err != nil {
+			RenderError(w, r, "Database Error", http.StatusInternalServerError)
+			return
+		}
+	}
 	if r.Method == http.MethodPost {
 		postID := r.FormValue("post_id")
 		comment := r.FormValue("comment")
